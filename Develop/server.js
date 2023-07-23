4164d0fa2d12e824fc37af7e0cc1c85611b6cfd4
Mark Anthony <manthonyamaro@yahoo.com>
2023-07-23 13:17:06 -0500

    initiated testing using consolel ogs to display connection erros and set pat to the swquelize data using category routes and verify connection has been made by setting the routes for post,get,put,delete

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,11 @@
+require('dotenv').config();
+
 const express = require('express');
-const routes = require('./routes');
-// import sequelize connection
+const productRoutes = require('./routes/api/product-routes');
+const tagRoutes = require('./routes/api/tag-routes');
+
+
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,9 +13,13 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+app.use('/api/products', productRoutes);
+app.use('/api/tags', tagRoutes);
+
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
