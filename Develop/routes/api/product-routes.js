fe76a2d482a14ff4a464fcc8064b156fd4335ebc
Mark Anthony <manthonyamaro@yahoo.com>
2023-08-12 18:58:38 -0500

    reviewed feedback and added The POST and PUT routes for products while including more of the instrucitons in the video to be placed in the readme

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -11,7 +11,14 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
-
+router.post('/', async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 router.get('/:id', async (req, res) => {
   try {
     const product = await Product.findByPk(req.params.id, {
@@ -23,7 +30,21 @@ router.get('/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
+router.put('/:id', async (req, res) => {
+  try {
+    const product = await Product.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
 
+    if (!product[0]) res.status(404).json({ message: 'No product found with this id!' });
+    else res.status(200).json(product);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 router.delete('/:id', async (req, res) => {
   try {
     const product = await Product.destroy({ where: { id: req.params.id } });
